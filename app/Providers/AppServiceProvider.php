cc4f3ba6b25c3f785b4c762505d90f076ccd1427
vagrant <vagrant@homestead>
2019-11-14 09:20:43 +0000

    fix varchar = 191

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,5 @@ class AppServiceProvider extends ServiceProvider
     {
         //
 	Schema::defaultStringLength(191);
-
     }
 }
